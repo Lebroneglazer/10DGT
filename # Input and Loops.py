6e9d759f5f69632dd32d9c1efcfd6313dc675d81
Lebroneglazer <ian.gan@kingsway.school.nz>
2025-05-07 11:31:48 +1200

    Add files via upload

---
+++ # Input and Loops.py
@@ -0,0 +1,26 @@
+# Input and Loops 
+# Aurthor: Ian Gan
+# Date 7/05/2025 
+# Version 1.0
+
+# Ask the user a question and store their answer in a variable 
+# Ask the user for their name and store it
+name = input("what is your name?")
+
+# Ask the user for two numbers and add them together.
+num1 = int(input("what is your first number?"))
+num2 = int(input("what is your second number?"))
+print(f"You first number was {num1}")
+print(f"You second number was {num2}")
+
+# To check that the input was stored correctly
+print(num1)
+print(num2)
+
+# Adding the input together
+sum = num1 + num2
+print(sum)
+
+# Test that the input was stored correctly
+print(name)
+# to comment code out, use ctrl + /
