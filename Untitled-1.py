cd73662c73b8b3471767d59ac9cde5a3ccd1ba8f
Lebroneglazer <ian.gan@kingsway.school.nz>
2025-05-07 10:15:47 +1200

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,18 @@
+# storing animals information: Name, height, number of legs and whether it has a tail or not
+# Ian Gan 
+# Date: 2 may 2025
+# Version 1
+
+animal_name = "elephant"
+num_legs = 4
+animal_height =4.5
+has_tail = True
+# Add another two or more parameters 
+# about the animal and display them 
+
+# Display animals details 
+print(f"The chosen animal is called {animal_name}.")
+print(f"The{animal_name} has {num_legs} legs ")
+print(f"It is {animal_height}m high.")
+print(f"The {has_tail} that the {animal_name} has a tail.")
+print(f"The {animal_name}has a tail: {has_tail}.")
